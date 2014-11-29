91d9084cfcabb6714d85162bf6ebb7d3ae1582f1
Svetlyi <svetlyi62@gmail.com>
2014-11-29 13:49:21 +0300

    count

---
+++ Assets/MainApplication.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainApplication : MonoBehaviour {
+
+	static private int count=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	static public void countInc() {
+		count++;
+	}
+
+	void OnGUI() {
+		Rect rect = new Rect (10,10,150,100);
+		GUI.Label(rect, "Count: " + count);
+	}
+}
--- Assets/gameLogic.cs
+++ Assets/gameLogic.cs
@@ -16,19 +16,10 @@ public class gameLogic : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.name == "CubeGoal") {
-			//Random(10);
 			Vector3 newPos = new Vector3(Random.Range(-4.0F, 2.0F), 4, Random.Range(-8.0F, 0F));
-
+			MainApplication.countInc();
 			Instantiate(this.prefab, newPos, transform.rotation);
 			Destroy(this.gameObject);
-			this.count++;
-			Debug.Log (this.count);
-			this.OnGUI();
 		}
-	}
-
-	void OnGUI() {
-		Rect rect = new Rect (10,10,150,100);
-		GUI.Label(rect, "Count: " + this.count);
 	}
 }
