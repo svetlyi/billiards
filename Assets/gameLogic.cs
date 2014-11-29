e28e593b666cb3756cab7655f628d97e0f73c0b4
Svetlyi <svetlyi62@gmail.com>
2014-11-29 13:24:32 +0300

    firstcommit

---
+++ Assets/gameLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameLogic : MonoBehaviour {
+	private int count = 0;
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "CubeGoal") {
+			//Random(10);
+			Vector3 newPos = new Vector3(Random.Range(-4.0F, 2.0F), 4, Random.Range(-8.0F, 0F));
+
+			Instantiate(this.prefab, newPos, transform.rotation);
+			Destroy(this.gameObject);
+			this.count++;
+			Debug.Log (this.count);
+			this.OnGUI();
+		}
+	}
+
+	void OnGUI() {
+		Rect rect = new Rect (10,10,150,100);
+		GUI.Label(rect, "Count: " + this.count);
+	}
+}
---
+++ Assets/keyHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyHandler : MonoBehaviour {
+	public int speedForce = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("right")) {
+			this.gameObject.rigidbody.AddForce(0, 0, speedForce);
+		}
+		if (Input.GetKey ("left")) {
+			this.gameObject.rigidbody.AddForce(0, 0, -speedForce);
+		}
+		if (Input.GetKey ("up")) {	
+			this.gameObject.rigidbody.AddForce(-speedForce, 0, 0);
+		}
+		if (Input.GetKey ("down")) {
+			this.gameObject.rigidbody.AddForce(speedForce, 0, 0);
+		}
+		if (Input.GetKey ("space")) {
+			this.gameObject.rigidbody.AddForce(0, 0, speedForce);
+		}
+	}
+}
